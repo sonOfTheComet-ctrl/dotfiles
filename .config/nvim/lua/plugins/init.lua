b3acf5307040d3cfeb796405988ee0ac3d7146d6
sonOfTheComet-ctrl <sonofthecometfinn@gmail.com>
2026-09-19 23:05:13 +0200

    polish: switch to catppuccin
    Signed-off-by: sonOfTheComet-ctrl <sonofthecometfinn@gmail.com>

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -444,15 +444,24 @@ return {
   },
 
   -- Colorscheme
+  --{
+  --  'sainnhe/gruvbox-material',
+  --  lazy = false,
+  --  priority = 1000,
+  --  config = function()
+  --    -- Optionally configure and load the colorscheme
+  --    -- directly inside the plugin declaration.
+  --    vim.g.gruvbox_material_enable_italic = true
+  --    vim.cmd.colorscheme 'gruvbox-material'
+  --  end,
+  --},
+  -- Catppuccin because I like to switch
   {
-    'sainnhe/gruvbox-material',
-    lazy = false,
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = true
-      vim.cmd.colorscheme 'gruvbox-material'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
